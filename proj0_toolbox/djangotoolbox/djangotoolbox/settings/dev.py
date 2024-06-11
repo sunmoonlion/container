651de504c6f5ae1c5b1d7e73bb2544ce05e948b4
sunmoonlion <13701819268@163.com>
2024-06-11 11:26:30 +0800

    add again4

--- proj0_toolbox/djangotoolbox/djangotoolbox/settings/dev.py
+++ proj0_toolbox/djangotoolbox/djangotoolbox/settings/dev.py
@@ -86,11 +86,50 @@ WSGI_APPLICATION = 'djangotoolbox.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': {#写（主机）
+        'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
+        'HOST': '47.103.135.26',  # 数据库主机
+        'PORT': 3300,  # 数据库端口
+        'USER': 'myrt',  # 数据库用户名
+        'PASSWORD': '123456',  # 数据库用户密码
+        'NAME': 'mydb'  # 数据库名字
+    },
+    'slave': {#读（从机）
+        'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
+        'HOST': '47.100.19.119',  # 数据库主机
+        'PORT': 4300,  # 数据库端口
+        'USER': 'myrt',  # 数据库用户名
+        'PASSWORD': '123456',  # 数据库用户密码
+        'NAME': 'mydb' # 数据库名字
     }
 }
+
+CACHES = {
+    "default": {  # 默认
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://47.103.135.26:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    "session": {  # session
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://47.103.135.26:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    "verify_code": {  # 验证码
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://47.103.135.26:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "session"
 
 
 # Password validation
@@ -112,6 +151,49 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# 配置工程日志
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,  # 是否禁用已经存在的日志器
+    'formatters': {  # 日志信息显示的格式
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(lineno)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(module)s %(lineno)d %(message)s'
+        },
+    },
+    'filters': {  # 对日志进行过滤
+        'require_debug_true': {  # django在debug模式下才输出日志
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {  # 日志处理方法
+        'console': {  # 向终端中输出日志
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file': {  # 向文件中输出日志
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': BASE_DIR / 'logs/mydb.log',  # 日志文件的位置
+            'maxBytes': 300 * 1024 * 1024,
+            'backupCount': 10,
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {  # 日志器
+        'django': {  # 定义了一个名为django的日志器
+            'handlers': ['console', 'file'],  # 可以同时向终端与文件中输出日志
+            'propagate': True,  # 是否继续传递日志信息
+            'level': 'INFO',  # 日志器接收的最低日志级别
+        },
+    }
+}
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -127,8 +209,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# 配置静态文件加载路径
+STATICFILES_DIRS = [BASE_DIR / 'djangotoolbox/static']
 
+# 设置STATIC ROOT 和 STATIC URL（注意，收集时的目录和开发时的存储目录（也是加载目录）是不同的，这点不同于媒体文件）
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "/static/"
+
+# 设置MEDIA ROOT 和 MEDIA URL(请注意：这里meidia_root指定的目录是收集时的目录，因为它就是开发阶段的媒体默认存储目录，所以，收集时没有变化)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
